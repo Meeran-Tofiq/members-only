be85c834ccb5c876bf4231e1ba1bcb888a93ac32
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-12 22:07:04 +0300

    Refactor title and content of message to be more strict

--- models/message.js
+++ models/message.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const message = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-	title: { type: String, required: true, minLength: 3 },
-	content: { type: String, required: true, maxLength: 250 },
+	title: { type: String, required: true, minLength: 3, maxLength: 75 },
+	content: { type: String, required: true, minLength: 5, maxLength: 250 },
 	date: { type: Date, default: Date.now() },
 });
 
