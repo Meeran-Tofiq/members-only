88ca027d00a9d474ecd9044096fc65e050dc4519
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-13 00:14:06 +0300

    Feat virtual to return a message's url

--- models/message.js
+++ models/message.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const message = new Schema({
-	user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-	title: { type: String, required: true, minLength: 3, maxLength: 75 },
-	content: { type: String, required: true, minLength: 5, maxLength: 250 },
-	date: { type: Date, default: Date.now() },
-});
+const message = new Schema(
+	{
+		user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		title: { type: String, required: true, minLength: 3, maxLength: 75 },
+		content: { type: String, required: true, minLength: 5, maxLength: 250 },
+		date: { type: Date, default: Date.now() },
+	},
+	{
+		virtuals: {
+			url: {
+				get() {
+					return "/message/" + this._id;
+				},
+			},
+		},
+	}
+);
 
 module.exports = mongoose.model("Message", message);
