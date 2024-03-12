2afd80b1cea66b3203a7604463b1b3a81d357e73
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-03-12 22:15:46 +0300

    Feat handling POST on making a message with validation

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user");
 const Message = require("../models/message");
+const { body, validationResult } = require("express-validator");
 
 exports.getMessageList = asyncHandler(async (req, res, next) => {
 	let messageList;
@@ -21,9 +22,35 @@ exports.getCreateMessage = asyncHandler(async (req, res, next) => {
 	});
 });
 
-exports.postCreateMessage = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: create message post");
-});
+exports.postCreateMessage = [
+	body("title", "Title must be between 3 and 75 characters")
+		.trim()
+		.isLength({ min: 3, max: 75 })
+		.escape(),
+	body("content", "Content must be between 25 and 250 characters")
+		.trim()
+		.isLength({ min: 5, max: 250 })
+		.escape(),
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+		const message = new Message({
+			user: req.user.id,
+			title: req.body.title,
+			content: req.body.content,
+		});
+
+		if (!errors.isEmpty()) {
+			res.render("message_form", {
+				title: "Create a Message",
+				message,
+				errors: errors.array(),
+			});
+		}
+
+		await message.save();
+		res.redirect("/");
+	}),
+];
 
 exports.getEditMessage = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: edit message page");
